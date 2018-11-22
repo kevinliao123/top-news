470ba6780c476b6d0eeaf5e1ff2a742fbb527840
he liao <he.liao8828@gmail.com>
2018-11-22 14:27:34 -0500

    React front end & node.js backend

---
+++ web_server/client/build/precache-manifest.4aba32ac215693b54478f349622de25b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f76c8a6f89e138d52bad",
+    "url": "/static/css/main.1c578564.chunk.css"
+  },
+  {
+    "revision": "f76c8a6f89e138d52bad",
+    "url": "/static/js/main.f76c8a6f.chunk.js"
+  },
+  {
+    "revision": "60865920cd389bfaf2a1",
+    "url": "/static/js/1.60865920.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "415185ceb209640525c1172acb45606e",
+    "url": "/index.html"
+  }
+];
---
+++ web_server/client/src/App/App.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import logo from '../logo.svg';
+import './App.css';
+import NewsPanel from "../NewsPanel/NewsPanel";
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <img className='logo' src={logo} alt='logo'/>
+                <div className='container'>
+                    <NewsPanel/>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
